9c16c50442ca1f4e99bd8270b8df2a17400d589b
Marcos Henrique <henrique.marcos1@escolar.ifrn.edu.br>
2026-07-09 21:25:07 -0300

    Adicionando integração com a openweather

--- clima/clima/clima.module.ts
+++ clima/clima/clima.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ClimaService } from './clima.service';
 import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, ConfigModule.forRoot({
+    isGlobal: true,
+  })],
   providers: [ClimaService],
   // outros módulos usem o serviço  
   exports: [ClimaService],
--- clima/clima/clima.service.ts
+++ clima/clima/clima.service.ts
@@ -9,13 +9,21 @@ export class ClimaService {
     private configService: ConfigService,
   ) {}
   // busca o clima de uma cidade usando a API do OpenWeatherMap
-  async buscarClima(cidade: string) {
+ async buscarClima(cidade: string) {
+  try {
     const apiKey = this.configService.get<string>('OPENWEATHER_API_KEY');
+
     const resposta = await this.httpService.axiosRef.get(
       `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}`,
     );
+
     return resposta.data;
-  }catch(error : unknown) {
-    return {mensagem: 'Erro ao buscar o clima no momento', error: (error as Error).message};  
+
+  } catch(error: unknown) {
+    return {
+      mensagem: 'Erro ao buscar o clima no momento',
+      error: (error as Error).message
+    };
   }
+}
 }
